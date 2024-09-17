705a3b9b391d8b5c19b57e6c55235d452bd0a488
Ash Madden <ashleymadden55@gmail.com>
2024-09-18 00:15:54 +1000

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+		colors: {
+			'dark-purple': '#301534',
+			pink: '#ad28eb',
+			'light-pink': '#f8eeff',
+			'pale-purple': '#8b6990',
+			white: '#ffffff',
+		},
+	},
+	plugins: [],
+};
